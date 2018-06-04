9be8292f90e607566b51db316ca3c91f40c88f48
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-06-04 19:03:35 +0100

    logic for bad api call

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,3 +1,6 @@
+require "net/https"
+require "uri"
+
 class GithubController < ApplicationController
   before_action :fetch_ghub_repos, only: [:show, :destroy]
 
@@ -15,14 +18,55 @@ class GithubController < ApplicationController
 
   def fetch_ghub_repos
     ghub_user = params[:user]
-    @user = Octokit.user ghub_user
-    ghub_repos = @user.rels[:repos].get.data
 
-    languages_array = []
-    ghub_repos.each do |repo|
-      languages_array.push(repo["language"])
+    # if working_url?(ghub_user)
+      if url?(ghub_user)
+      @user = Octokit.user ghub_user
 
+      ghub_repos = @user.rels[:repos].get.data
+
+      languages_array = []
+      ghub_repos.each do |repo|
+        languages_array.push(repo["language"])
+      end
       @fav_language = languages_array.max_by { |i| languages_array.count(i) }
     end
   end
+    # end
+
+
+
+  def url?(ghub_user)
+    uri = URI.parse("https://github.com/#{ghub_user}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    res = http.request(request)
+
+    @status = res.code
+
+    @status == "200" ? true : false
+  end
+
+
+# def working_request?(ghub_user)
+# open("https://github.com/#{ghub_user}") do |g|
+#   g.base_uri  #=> http://www.example.org
+#   g.status #=> ["200", "OK"]
+#   raise
+# end
+# end
+
+  # def working_url?(ghub_user)
+  #   ghub_url = "https://github.com/#{ghub_user}"
+  #   ghub_url = URI.parse(ghub_url)
+  #   Net::HTTP.start(ghub_url.host, ghub_url.port) do |http|
+  #     http.head(ghub_url.request_uri).code == '200'
+
+  #   end
+  # rescue
+  #   false
+  # end
+
 end
